812d940494ba3bb6040a090e75d2950d0924b161
Sebastian Markbage <sebastian@calyptus.eu>
2011-04-24 21:06:28 +0200

    Arguments 0 used for inheritance

--- Source/AST.Expression.js
+++ Source/AST.Expression.js
@@ -1,6 +1,6 @@
 AST.Expression = function(obj){
-	if (!obj) return;
-	if (typeof obj.toExpression == 'function') obj = obj.toExpression();
+	if (arguments.length == 0) return;
+	if (obj && typeof obj.toExpression == 'function') obj = obj.toExpression();
 	return obj instanceof AST.Expression ? obj : new AST.Literal(obj);
 };
 
